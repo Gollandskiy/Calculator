aa99b9ff137f628945b00d63ed8dbe35ce50707d
nikita.prigolovkin <vitalii.prigolovkin@gmail.com>
2023-07-11 04:44:39 +0300

    Update1

---
+++ CalculationHistoryWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    public partial class CalculationHistoryWindow : Window
+    {
+        public ObservableCollection<Calculation> CalculationHistory { get; } = new ObservableCollection<Calculation>();
+        public Calculation CurrentCalculation { get; set; } = new Calculation();
+
+        public CalculationHistoryWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public class Calculation
+        {
+            public int Counter { get; set; }
+            public string Result { get; set; }
+        }
+
+        public void AddCalculation(Calculation calculation)
+        {
+            CalculationHistory.Add(calculation);
+
+            if (CalculationHistory.Count > 5)
+            {
+                CalculationHistory.RemoveAt(0);
+            }
+
+            CurrentCalculation = calculation;
+        }
+
+        public void UpdateData(ObservableCollection<Calculation> calculations)
+        {
+            CalculationHistory.Clear();
+
+            int counter = 1;
+            foreach (var calculation in calculations)
+            {
+                calculation.Counter = counter++;
+                CalculationHistory.Add(calculation);
+            }
+        }
+    }
+}
+
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,14 +14,18 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
 using System.Reflection.Emit;
+using static Calculator.CalculationHistoryWindow;
+using System.Collections.ObjectModel;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        private CalculationHistoryWindow calculationHistoryWindow;
+        private ObservableCollection<Calculation> calculations = new ObservableCollection<Calculation>();
+        private int calculationCounter = 1;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,17 +40,58 @@ namespace Calculator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string str = (string)((Button)e.OriginalSource).Content;
-            if (str == "Clear")
-                Lab1.Text = "";
-
-            else if (str == "=")
+            try
             {
-                string data = new DataTable().Compute(Lab1.Text, null).ToString();
-                Lab1.Text = data;
+                string str = (string)((Button)e.OriginalSource).Content;
+                if (str == "Clear")
+                    Lab1.Text = "";
+
+                else if (str == "=")
+                {
+                    string data = new DataTable().Compute(Lab1.Text, null).ToString();
+                    Lab1.Text = data;
+                    UpdateCalculationHistory(data);
+                }
+                else if (str == "<<")
+                {
+                    if (Lab1.Text.Length > 0)
+                        Lab1.Text = Lab1.Text.Substring(0, Lab1.Text.Length - 1);
+                }
+                else if (str == "History")
+                    Lab1.Text = "";
+                else
+                    Lab1.Text += str;
             }
-            else
-                Lab1.Text += str;
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
- }
+        private void UpdateCalculationHistory(string result)
+        {
+            Calculation calculation = new Calculation { Counter = calculationCounter, Result = result };
+            calculations.Insert(0, calculation);
+
+            if (calculations.Count > 5)
+            {
+                calculations.RemoveAt(5);
+            }
+
+            calculationCounter = Math.Min(calculationCounter + 1, 5);
+
+            if (calculationHistoryWindow != null && calculationHistoryWindow.IsVisible)
+            {
+                calculationHistoryWindow.UpdateData(calculations);
+            }
+        }
+        private void ShowCalculationHistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (calculationHistoryWindow == null || !calculationHistoryWindow.IsVisible)
+            {
+                calculationHistoryWindow = new CalculationHistoryWindow();
+                calculationHistoryWindow.UpdateData(calculations);
+                calculationHistoryWindow.Show();
+            }
+        }
+    }
+}
